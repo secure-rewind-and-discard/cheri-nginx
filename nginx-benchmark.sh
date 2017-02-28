237f9865a09ec279501f70636e385b29ab34a742
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-02-28 18:07:16 +0000

    When running the benchmark, show the file information, so it's obvious what binary is being benchmarked.  Also stop the daemon after we're done.

--- nginx-benchmark.sh
+++ nginx-benchmark.sh
@@ -1,7 +1,14 @@
 #!/bin/sh -e
 
+NGINX=/usr/local/nginx/sbin/nginx
+FETCHBENCH=/usr/local/nginx/sbin/fetchbench
+
+echo "${0}: nginx binary details:"
+file ${NGINX}
 echo "${0}: starting nginx..."
-/usr/local/nginx/sbin/nginx
+${NGINX}
 echo "${0}: running benchmark..."
-/usr/local/nginx/sbin/fetchbench http://127.0.0.1/ 5 200
+${FETCHBENCH} http://127.0.0.1/ 5 200
+echo "${0}: stopping nginx..."
+${NGINX} -s stop
 echo "${0}: done"
