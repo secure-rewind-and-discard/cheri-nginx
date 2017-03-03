0e430c8274c605ed7334d5a8527429f73c653368
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-03-03 14:47:56 +0000

    Make it build with lld.  This requires -mxgot, otherwise it crashes at startup.

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -9,7 +9,7 @@ INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
 # the output binary and the build will fail.
 sh auto/configure --with-debug --without-pcre --without-http_rewrite_module
 
-COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -O2 -msoft-float -ggdb -static -integrated-as"
+COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -mxgot -O2 -msoft-float -ggdb -static -integrated-as"
 COMPILE_FLAGS="${COMMON_FLAGS} -Wcheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 
 export CFLAGS=${COMPILE_FLAGS}
@@ -17,7 +17,7 @@ export CFLAGS=${COMPILE_FLAGS}
 export PATH=${CHERISDK}:${CHERILDDIR}:$PATH
 export CC=${CHERISDK}/clang
 
-sed -i '' "s!^CC .*!CC = ${CC}!;s!^CFLAGS .*!CFLAGS = ${CFLAGS}!;s!^CPP .*!CPP = ${CC} -E!;s!^LINK .*!LINK = ${CC} ${CFLAGS}!" objs/Makefile
+sed -i '' "s!^CC .*!CC = ${CC}!;s!^CFLAGS .*!CFLAGS = ${CFLAGS}!;s!^CPP .*!CPP = ${CC} -E!;s!^LINK .*!LINK = ${CC} ${CFLAGS} -fuse-ld=lld!" objs/Makefile
 make -j8
 make install DESTDIR=${INSTALL_DIR}
 chmod -R a+rX ${INSTALL_DIR}
