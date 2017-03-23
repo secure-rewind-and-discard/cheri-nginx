53d2331bf7514e46a4dffa220bb632f393c14bc1
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-03-23 11:57:34 +0000

    s/sandbox/purecap/

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -9,7 +9,7 @@ INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
 # the output binary and the build will fail.
 sh auto/configure --with-debug --without-pcre --without-http_rewrite_module
 
-COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=sandbox -mxgot -O2 -msoft-float -ggdb -static -integrated-as"
+COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=purecap -mxgot -O2 -msoft-float -ggdb -static -integrated-as"
 COMPILE_FLAGS="${COMMON_FLAGS} -Wcheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 
 export CFLAGS=${COMPILE_FLAGS}
