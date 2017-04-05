c729ff7d6404dc50dd8faa7b915550752216339f
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-04-05 11:32:45 +0100

    Run benchmark ten times.

--- nginx-benchmark.sh
+++ nginx-benchmark.sh
@@ -2,13 +2,16 @@
 
 NGINX=/usr/local/nginx/sbin/nginx
 FETCHBENCH=/usr/local/nginx/sbin/fetchbench
+NTIMES=10
 
 echo "${0}: nginx binary details:"
 file ${NGINX}
 echo "${0}: starting nginx..."
 ${NGINX}
-echo "${0}: running benchmark..."
-${FETCHBENCH} http://127.0.0.1/ 5 200
+echo "${0}: running benchmark ${NTIMES} times..."
+for i in `jot ${NTIMES}`; do
+	${FETCHBENCH} http://127.0.0.1/ 5 200
+done
 echo "${0}: stopping nginx..."
 ${NGINX} -s stop
 echo "${0}: done"
