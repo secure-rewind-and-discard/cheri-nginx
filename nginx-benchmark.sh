e270ed8c8b659f8ee346cccf357ffe937ff99dcd
Edward Tomasz Napierala <trasz@FreeBSD.org>
2017-04-12 11:58:46 +0100

    Make nginx-benchmark.sh print additional information about the kernel.

--- nginx-benchmark.sh
+++ nginx-benchmark.sh
@@ -4,14 +4,24 @@ NGINX=/usr/local/nginx/sbin/nginx
 FETCHBENCH=/usr/local/nginx/sbin/fetchbench
 NTIMES=10
 
+echo "${0}: uname:"
+uname -a
+
+echo "${0}: invariants/witness:"
+sysctl -a | grep -E '(invariants|witness)'
+
 echo "${0}: nginx binary details:"
 file ${NGINX}
+
 echo "${0}: starting nginx..."
 ${NGINX}
+
 echo "${0}: running benchmark ${NTIMES} times..."
 for i in `jot ${NTIMES}`; do
 	${FETCHBENCH} http://127.0.0.1/ 5 200
 done
+
 echo "${0}: stopping nginx..."
 ${NGINX} -s stop
+
 echo "${0}: done"
