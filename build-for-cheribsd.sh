39d4f95aa2d944d4964d6ea3865b378d67e478f3
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2017-04-25 11:04:37 +0000

    Allow overriding variables

--- build-for-cheribsd.sh
+++ build-for-cheribsd.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 case "$1" in
 	"cheri"|"")
@@ -12,10 +12,10 @@ case "$1" in
 		exit 1
 esac
 
-CHERI_ROOT="${HOME}/cheri"
-CHERISDK="${CHERI_ROOT}/output/sdk256/bin"
-CHERIBSD_SYSROOT="${CHERI_ROOT}/output/sdk256/sysroot"
-INSTALL_DIR=${CHERI_ROOT}/output/rootfs256
+CHERI_ROOT=${CHERI_ROOT:-${HOME}/cheri}
+CHERISDK=${CHERISDK:-${CHERI_ROOT}/output/sdk256/bin}
+CHERIBSD_SYSROOT=${CHERIBSD_SYSROOT:-${CHERI_ROOT}/output/sdk256/sysroot}
+INSTALL_DIR=${INSTALL_DIR:-${CHERI_ROOT}/output/rootfs256}
 
 # This needs to be done first.  Nginx doesn't support cross-building;
 # if you try to tell it to use cheri compiler, it will try to execute
@@ -25,10 +25,10 @@ sh auto/configure --with-debug --without-pcre --without-http_rewrite_module
 COMMON_FLAGS="-pipe --sysroot=${CHERIBSD_SYSROOT} -B${CHERISDK} -target cheri-unknown-freebsd -mabi=${MABI} -mxgot -O2 -msoft-float -ggdb -static -integrated-as"
 COMPILE_FLAGS="${COMMON_FLAGS} -Wcheri-capability-misuse -Werror=implicit-function-declaration -Werror=format -Werror=undefined-internal"
 
-export CFLAGS=${COMPILE_FLAGS}
-export CFLAGS=${COMPILE_FLAGS}
-export PATH=${CHERISDK}:${CHERILDDIR}:$PATH
-export CC=${CHERISDK}/clang
+export CFLAGS="${COMPILE_FLAGS}"
+export CFLAGS="${COMPILE_FLAGS}"
+export PATH="${CHERISDK}:${CHERILDDIR}:$PATH"
+export CC="${CHERISDK}/clang"
 
 #sed -i '' "s!^CC .*!CC = ${CC}!;s!^CFLAGS .*!CFLAGS = ${CFLAGS}!;s!^CPP .*!CPP = ${CC} -E!;s!^LINK .*!LINK = ${CC} ${CFLAGS} -fuse-ld=lld!;s!-Wl,-E!!" objs/Makefile
 sed -i '' "s!^CC .*!CC = ${CC}!;s!^CFLAGS .*!CFLAGS = ${CFLAGS}!;s!^CPP .*!CPP = ${CC} -E!;s!^LINK .*!LINK = ${CC} ${CFLAGS}!;s!-Wl,-E!!" objs/Makefile
