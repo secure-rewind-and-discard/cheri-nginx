053581ec6b6957af07896f24fdeb97f00c01ea93
Alexander Richardson <arichardson.kde@gmail.com>
2017-08-24 12:15:33 +0100

    Make tests configurable

--- run-nginx-tests.sh
+++ run-nginx-tests.sh
@@ -3,9 +3,11 @@
 # pkg bootstrap && pkg install -y perl5 p5-TAP-Formatter-JUnit
 which perl
 which prove
-NGINX_DIR=$(realpath .)
+NGINX_DIR="${NGINX_DIR:-$(realpath .)}"
+NGINX_TESTS_DIR="${NGINX_TESTS_DIR:-/opt/nginx-tests}"
 export TEST_NGINX_BINARY=${NGINX_DIR}/sbin/nginx
 export TEST_NGINX_VERBOSE=1
 export TEST_NGINX_CATLOG=1
 # http://blogs.perl.org/users/confuseacat/2011/09/perl-testing-with-jenkinshudson-avoiding-some-pitfalls.html
-prove -v --formatter=TAP::Formatter::JUnit /opt/nginx-tests | tee /tmp/test_output.xml
+# prove -v --formatter=TAP::Formatter::JUnit /opt/nginx-tests | tee /tmp/test_output.xml
+prove -v .
