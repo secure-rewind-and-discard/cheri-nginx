a68ca2093d9ef583347c23985888f7a24dcd1c1e
Alexander Richardson <arichardson.kde@gmail.com>
2017-08-24 10:32:02 +0100

    Don't use pkg, we just copy the files over via scp
    Should be faster

--- run-nginx-tests.sh
+++ run-nginx-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -xe
 # TODO: run the NGINX test suite
-pkg bootstrap && pkg install -y perl5 p5-TAP-Formatter-JUnit
+# pkg bootstrap && pkg install -y perl5 p5-TAP-Formatter-JUnit
+which perl
+which prove
 NGINX_DIR=$(realpath .)
 export TEST_NGINX_BINARY=${NGINX_DIR}/sbin/nginx
 # http://blogs.perl.org/users/confuseacat/2011/09/perl-testing-with-jenkinshudson-avoiding-some-pitfalls.html
