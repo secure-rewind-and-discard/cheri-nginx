3c8ad2c3789028202383401e220da71e85d1f749
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-06-28 11:51:40 +0100

    Add alloc_size attribute

--- src/core/ngx_palloc.h
+++ src/core/ngx_palloc.h
@@ -72,17 +72,17 @@ typedef struct {
 } ngx_pool_cleanup_file_t;
 
 
-void *ngx_alloc(size_t size, ngx_log_t *log);
-void *ngx_calloc(size_t size, ngx_log_t *log);
+void *ngx_alloc(size_t size, ngx_log_t *log) __attribute__((alloc_size(1)));
+void *ngx_calloc(size_t size, ngx_log_t *log) __attribute__((alloc_size(1)));
 
 ngx_pool_t *ngx_create_pool(size_t size, ngx_log_t *log);
 void ngx_destroy_pool(ngx_pool_t *pool);
 void ngx_reset_pool(ngx_pool_t *pool);
 
-void *ngx_palloc(ngx_pool_t *pool, size_t size);
-void *ngx_pnalloc(ngx_pool_t *pool, size_t size);
-void *ngx_pcalloc(ngx_pool_t *pool, size_t size);
-void *ngx_pmemalign(ngx_pool_t *pool, size_t size, size_t alignment);
+void *ngx_palloc(ngx_pool_t *pool, size_t size) __attribute__((alloc_size(2)));
+void *ngx_pnalloc(ngx_pool_t *pool, size_t size) __attribute__((alloc_size(2)));
+void *ngx_pcalloc(ngx_pool_t *pool, size_t size) __attribute__((alloc_size(2)));
+void *ngx_pmemalign(ngx_pool_t *pool, size_t size, size_t alignment) __attribute__((alloc_size(2)));
 ngx_int_t ngx_pfree(ngx_pool_t *pool, void *p);
 
 
